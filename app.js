c7ac0e73414022982bcc9a7fc38477552fc834e2
Sudhakar Swain <sudhakar.swain@delphic.in>
2023-03-24 22:46:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,187 @@
+const express = require('express');
+const path = require('path');
+const hbs = require("hbs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const app = express();
+const session = require("express-session")
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const bcryptjs = require("bcryptjs");
+const crypto = require('crypto');
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname , 'views'))
+
+app.use(session({
+  secret: 'mysecretkey',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Serve the note page
+app.get('/', (req, res) => {
+  res.status(200).render('note');
+});
+
+app.get("/signup", (req ,res)=>{
+    res.render('signup')
+})
+app.get("/login", (req ,res)=>{
+  res.render('login')
+});
+app.get("/forgot_password", (req ,res)=>{
+  res.render('forgot_password')
+});
+app.get("/termsofuse", (req ,res)=>{
+  res.render('termsofuse')
+});
+app.get("/privacypolicy", (req ,res)=>{
+  res.render('privacypolicy')
+});
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://sudhakarswaindelphic:XrXjQ3jsbdGEsHHS@delphic1.mfomzhe.mongodb.net/scratchpad?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+// Define user model
+const User = mongoose.model('User', userSchema);
+
+// Use body-parser middleware to parse request body
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Signup route
+app.post('/signup', async (req, res) => {
+  // Check if email already exists
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) {
+    return res.status(400).send('Email already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create new user
+  const user = new User({
+    email: req.body.email,
+    password: hashedPassword,
+  });
+
+  // Save new user to database
+  try {
+    await user.save();
+    res.send('User created successfully');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+app.post('/signup', (req, res) => {
+  const idToken = req.body.idToken;
+  const CLIENT_ID = '398165584439-c8ah1893rb41it2vl1icqircp4ud83mg.apps.googleusercontent.com';
+  const client = new OAuth2Client(CLIENT_ID);
+
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: idToken,
+          audience: CLIENT_ID,
+      });
+      const payload = ticket.getPayload();
+      const { email, given_name, family_name } = payload;
+      // Save user data to database and send response
+  }
+  verify().catch((error) => console.log(error));
+});
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if email exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).send('Invalid email or password');
+  }
+
+  // Check if password is correct
+  const isPasswordValid = bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).send('/Invalid email or password');
+  }
+
+  res.redirect('/');
+  
+});
+
+
+// Define a route to handle the forgot password form submission
+app.post('/forgot-password', (req, res) => {
+  // Retrieve the email address from the request body
+  const email = req.body.email;
+
+  // Generate a password reset token
+  const token = generateToken();
+  
+
+function generateToken() {
+  // Generate a 32-byte random string
+  const buffer = crypto.randomBytes(32);
+
+  // Convert the buffer to a hex string
+  const token = buffer.toString('hex');
+
+  return token;
+}
+
+
+  // Store the token in the database, along with an expiration date
+  storeToken(email, token, new Date(Date.now() + 3600000));
+
+  // Create a nodemailer transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'jaquelin.brown@ethereal.email',
+        pass: 'KPPpteXQcKkxr6PmmV'
+    }
+});
+
+  // Define the email message
+  const mailOptions = {
+    from: 'getscratchpad.com>',
+    to: email,
+    subject: 'Reset Your Password',
+    text: `Click the link below to reset your password: ${process.env.APP_URL}/reset-password?token=${token}`,
+    html: `<p>Click the link below to reset your password:</p><p><a href="${process.env.APP_URL}/reset-password?token=${token}">${process.env.APP_URL}/reset-password?token=${token}</a></p>`
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Failed to send email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.render('forgot-password-confirmation', {email});
+    }
+  });
+});
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
---
+++ public/script.js
@@ -0,0 +1,156 @@
+const noteTextArea = document.getElementById('note');
+const panelMessage = document.getElementById('panel-message');
+const charCount = document.getElementById('char-count');
+const lineCount = document.getElementById('line-count');
+const wordCount = document.getElementById('word-count');
+const saveButton = document.getElementById("save-btn");
+const loginBtn = document.getElementById("login-btn");
+const overlay = document.getElementById("login-overlay");
+const loginForm = document.querySelector(".login-page form");
+
+
+let saveTimeoutId;
+
+// Load note from localStorage on page load
+window.addEventListener('online', () => {
+  const savedNote = localStorage.getItem('note');
+  if (savedNote){
+    fetch('/login',{
+      method : 'POST',
+      body : JSON.stringify({savedNote}),
+      headers:{
+        'Content-Type ': 'application/json'
+      }
+    }).then(()=>{
+      localStorage.removeItem("savedNote")
+    })
+  }
+
+});
+
+
+// Listen for input events on the textarea
+noteTextArea.addEventListener("input", function() {
+  const currentLength = this.value.length;
+
+  // Check if the current length is greater than or equal to 1000
+  if (currentLength >= 1000) {
+    alert("You have reached the limit of 1000 characters. Saving to the cloud would be beneficial. Storage is free as of now.");
+  }
+});
+loginBtn.addEventListener("click", function() {
+	overlay.style.display = "block";
+});
+let numTries = 0;
+
+function saveToCloud() {
+  if (numTries < 10) {
+    const text = document.getElementById('note').value;
+    if (text.length > 1000) {
+      numTries++;
+      return;
+    }
+  } else {
+    return;
+  }
+
+  window.location.href = '/login';
+}
+// Autosave note to localStorage after 500ms of not typing
+noteTextArea.addEventListener('input', () => {
+  clearTimeout(saveTimeoutId);
+  saveTimeoutId = setTimeout(() => {
+    const note = noteTextArea.value;
+    localStorage.setItem('note', note);
+    displayPanelMessage('Note saved.');
+  }, 500);
+});
+
+// Function to display a message on the panel
+function displayPanelMessage(message) {
+  panelMessage.innerText = message;
+  setTimeout(() => {
+    panelMessage.innerText = '';
+  }, 2000);
+}
+note.addEventListener('input', function() {
+  const text = note.value;
+
+  // Count number of characters
+  const numChars = text.length;
+  charCount.innerText = numChars;
+
+  // Count number of lines
+  const numLines = text.split('\n').length;
+  lineCount.innerText = numLines;
+
+  // Count number of words
+  const numWords = text.match(/\b\w+\b/g);
+  wordCount.innerText = numWords ? numWords.length : 0;
+});
+
+const termsLink = document.createElement('a');
+termsLink.href = 'http://localhost:3000/termsofuse';
+termsLink.innerText = 'Terms of Use';
+termsLink.style.position = 'fixed';
+termsLink.style.right = 0;
+document.body.appendChild(termsLink);
+
+const privacyLink = document.createElement('a');
+privacyLink.href = 'http://localhost:3000/privacypolicy';
+privacyLink.innerText = 'Privacy Policy';
+privacyLink.style.position ="fixed";
+privacyLink.style.right = 10;
+document.body.appendChild(privacyLink);
+
+function showLogin() {
+  const loginForm = document.getElementById('login-form');
+  loginForm.style.display = 'block';
+}
+
+function redirectToSignUp(){
+  window.location.href = "http://localhost:3000/signup"
+}
+
+function redirectToForgotPassword(){
+  window.location.href = "http://localhost:3000/forgot_password"
+}
+
+
+function isValidEmail(email) {
+  // Regular expression for validating email
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+function cancelSignUp(){
+  const cancel = document.getElementById("cancel")
+
+  cancel.reset();
+  
+  cancel.style.display = " none";
+}
+
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.addEventListener("click", cancelSignUp);
+
+const googleSignUpBtn = document.getElementById('google-signup-btn');
+    googleSignUpBtn.addEventListener('click', () => {
+        gapi.auth2.getAuthInstance().signIn().then((googleUser) => {
+            const idToken = googleUser.getAuthResponse().id_token;
+            // Send idToken to server for authentication
+            fetch('/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ idToken })
+            }).then((response) => {
+                if (response.ok) {
+                    window.location.href = 'http://localhost:3000/'; // Redirect to dashboard
+                } else {
+                    console.log('Google sign up failed');
+                }
+            });
+        });
+    });
